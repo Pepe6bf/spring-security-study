7300b8d0cdbdef027b5e4251ad41bbc71304f062
pepe6bf <pepe6bf@gmail.com>
2022-10-26 20:53:14 +0900

    Sec01 : #3. Form Login 인증
    - Form Login방식을 위한 각종 SecurityConfig 설정 방식에 대해 학습하였다.
    
    앞으로 내가 진행할 프로젝트는 거의 REST-API방식이기 때문에, Form-Login에 관련된 내용은 가볍게 들으면 좋을거 같다.

--- basicsecurity/src/main/java/com/security/study/basicsecurity/config/SecurityConfig.java
+++ basicsecurity/src/main/java/com/security/study/basicsecurity/config/SecurityConfig.java
@@ -1,10 +1,21 @@
 package com.security.study.basicsecurity.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -12,12 +23,36 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // 인증 설정
+        final Logger log = LoggerFactory.getLogger(getClass());
+
+        // 인증/인가 설정
         http
                 .authorizeRequests()
                 .anyRequest().authenticated();
         http
-                .formLogin();
+                .formLogin()
+//                .loginPage("/loginPage")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login")
+                .usernameParameter("userId")
+                .passwordParameter("passwd")
+                .loginProcessingUrl("/login_proc")
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        log.warn("authentication {}", authentication.getName());
+                        response.sendRedirect("/");
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                        log.warn("exception {}", exception.getMessage());
+
+                        response.sendRedirect("/login");
+                    }
+                })
+                .permitAll();
 
         return http.build();
     }
